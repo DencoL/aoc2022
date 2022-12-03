481ec70f181691e22227f3393c031a61a3a33c95
Denis <denis.lochman@protonmail.com>
2022-12-03 19:15:06 +0100

    day 3 some refactoring (i still hate the code for part two)

--- src/day_3/day_3.rs
+++ src/day_3/day_3.rs
@@ -1,4 +1,5 @@
 use crate::problem::Problem;
+use std::collections::HashSet;
 
 pub struct Day3 {}
 
@@ -7,26 +8,20 @@ impl Problem for Day3 {
         let mut common_items: Vec<char> = vec![];
 
         for rucksack in input.lines() {
-            let items: Vec<char> = rucksack.split("").flat_map(|i| i.chars()).collect();
-            let half_size = items.len() / 2;
+            let half_size = rucksack.len() / 2;
 
-            let first_half: &[char] = &items[..half_size];
-            let second_half: &[char] = &items[half_size..];
-
-            let mut already_added: Vec<&char> = vec![];
+            let (first_half, second_half) = rucksack.split_at(half_size);
+            let first_half: HashSet<char> = first_half.chars().collect();
+            let second_half: HashSet<char> = second_half.chars().collect();
+           
             for item in first_half {
-                if already_added.contains(&item) {
-                    continue;
-                }
-
-                if second_half.contains(item) {
-                    common_items.push(*item);
-                    already_added.push(&item);
+                if second_half.contains(&item) {
+                    common_items.push(item);
                 }
             }
         }
 
-        return get_items_priority_sum(common_items);
+        return get_items_priority_sum(common_items) as usize;
     }
 
     fn solve_part_two(&self, input: &str) -> usize {
@@ -66,7 +61,7 @@ impl Problem for Day3 {
             }
         }
 
-        return get_items_priority_sum(common_items);
+        return get_items_priority_sum(common_items) as usize;
     }
 
     fn index(&self) -> usize {
@@ -79,19 +74,18 @@ impl Problem for Day3 {
 }
 
 fn get_items_priority_sum(items: Vec<char>) -> usize {
+    const LOWER_PRIORITY_BOUNDARY: u8 = b'a' - 1;
+    const UPPER_PRIORITY_BOUNDARY: u8 = b'A' - 27;
+
     return items.iter().map(|item| {
-        let item_ascii = *item as usize;
+        let item_ascii = *item as u8;
 
-        let mut priority = 0;
-        if item_ascii >= 97 && item_ascii <= 122 {
-            priority = item_ascii - 96; 
+        let mut subtract_boundary = UPPER_PRIORITY_BOUNDARY;
+        if item.is_ascii_lowercase() {
+            subtract_boundary = LOWER_PRIORITY_BOUNDARY;
         }
 
-        if item_ascii >= 65 && item_ascii <= 90 {
-            priority = item_ascii - 38;
-        }
-
-        return priority;
+        return (item_ascii - subtract_boundary) as usize;
     }).sum();
 }
 
